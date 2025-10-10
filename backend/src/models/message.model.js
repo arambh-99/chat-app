5ab0df0c2522dec4a416b3fea3918d5d7bfa8860
arambh-99 <103411207+arambh-99@users.noreply.github.com>
2025-10-10 22:39:18 +0530

    User module completed

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    } ,
+    chat: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message
