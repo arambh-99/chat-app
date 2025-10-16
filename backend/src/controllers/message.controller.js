2615b57dccde8b382c1987f50f51bb98037d9245
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-10-16 14:05:31 +0530

    backend done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import Message from "../models/message.model.js";
 export const getUsersForSidebar = async(req, res) => {
     try {
          const loggedInUser = req.user._id;
@@ -13,11 +13,46 @@ export const getUsersForSidebar = async(req, res) => {
     }
 }
 
-export const getMessages = async (req,res)=>{
+export const getMessages = async (req,res)=>{  
     try {
         const {id:userToChatId} = req.params
-        const senderId = req.user._id
+        const myId = req.user._id
+
+        const messages = await Message.find({
+       $or: [
+        { senderId:myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId }
+     ]
+    })
+    res.status(200).json(messages)
     } catch (error) {
-        
+        console.log("Error in getMessages controller",error.message);
+        res.status(500).json({ error: "Internal Server Error" }); 
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text,image}=req.body
+        const {id:receiverId}=req.params
+        const senderId=req.user._id
+
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image)
+            imageUrl=uploadResponse.secure_url
+        }
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+       await newMessage.save()
+        //todo: realtime functionality goes here=>socket.io
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in send message controller",error.message)
+        res.status(500).json({error:"Internal Server Error"})
     }
 }
