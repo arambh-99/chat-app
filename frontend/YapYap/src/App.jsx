808f064b5a97f5a8f4b33eb521a4b38cc7e8e4c9
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-10-25 14:34:00 +0530

    navbar created

--- frontend/YapYap/src/App.jsx
+++ frontend/YapYap/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 const App = () => {
-  return (
-    <div className="text-red-500">App</div>
+  return (<>
+  <div >
+    <Navbar/>
+    <Routes>
+<Route path='/' element={<HomePage />}/>
+<Route path='/signup' element={<SignUpPage />}/>
+<Route path='/login' element={<LoginPage />}/>
+<Route path='/settings' element={<SettingsPage />}/>
+<Route path='/profile' element={<ProfiePage />}/>
+    </Routes>
+  </div>
+</>
   )
 }
 
