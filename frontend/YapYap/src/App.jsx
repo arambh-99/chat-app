8d47c908d7facd737662f796c7dde1495eedd4f6
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-10-29 20:38:41 +0530

    some changes

--- frontend/YapYap/src/App.jsx
+++ frontend/YapYap/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
+import HomePage from './pages/Homepage.jsx'
+import SignUpPage from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfiePage from './pages/ProfilePage.jsx'
 const App = () => {
   return (<>
   <div >
