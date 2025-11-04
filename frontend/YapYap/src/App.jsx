a6e75df986016dc81a9272b000894cd463096773
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-11-04 15:23:24 +0530

    signin done

--- frontend/YapYap/src/App.jsx
+++ frontend/YapYap/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { Routes,Route } from 'react-router-dom'
+import {Loader} from 'lucide-react'
+import { Routes,Route,Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import HomePage from './pages/Homepage.jsx'
 import SignUpPage from './pages/SignupPage.jsx'
@@ -8,22 +9,32 @@ import LoginPage from './pages/LoginPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfiePage from './pages/ProfilePage.jsx'
 import { useAuthStore } from './store/useAuthStore.js'
+
 const App = () => {
-  const {authUser,checkAuth}=useAuthStore()
+  const {authUser,checkAuth,isCheckingAuth}=useAuthStore()
  useEffect(() => {
   checkAuth(); 
  },[checkAuth]);
 
  console.log({authUser});
+
+ //if authUser is null and isCheckingAuth is true
+ if(!authUser && isCheckingAuth)
+  return( <div className='flex items-center justify-center h-screen'>
+  <Loader className="size-10 animate-spin"/>
+ </div>); 
+
+
+
   return (
   <div >
     <Navbar/>
     <Routes>
-<Route path='/' element={<HomePage />}/>
-<Route path='/signup' element={<SignUpPage />}/>
-<Route path='/login' element={<LoginPage />}/>
+<Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
+<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />}/>
+<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />}/>
 <Route path='/settings' element={<SettingsPage />}/>
-<Route path='/profile' element={<ProfiePage />}/>
+<Route path='/profile' element={authUser ?<ProfiePage /> : <Navigate to="/login" />} />
     </Routes>
   </div>
 
