a06aa1c301a231db8a3c39d70a9ec10f5eb561ef
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-11-11 15:45:24 +0530

    finally fixed cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,6 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
-dotenv.config({ path: '../.env' }); 
-
 
 const port=process.env.PORT;
 
@@ -23,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
 
 app.listen(port, () => {
   console.log("server is running on port: "+ port);
