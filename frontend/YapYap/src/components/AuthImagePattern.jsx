8a3bf1ea457ebaa6841d695cdf5c2e8dedf09ea2
Arambh Suyal <zengardener@Arambhs-MacBook-Air.local>
2025-11-14 14:44:11 +0530

    settings page

--- frontend/YapYap/src/components/AuthImagePattern.jsx
+++ frontend/YapYap/src/components/AuthImagePattern.jsx
@@ -6,7 +6,7 @@ const AuthImagePattern = ({ title, subtitle }) => {
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
-              className={`aspect-square rounded-2xl bg-violet-700/10 ${
+              className={`aspect-square rounded-2xl bg-prim-700/10 ${
                 i % 2 === 0 ? "animate-pulse" : ""
               }`}
             />
